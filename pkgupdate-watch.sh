add9056c370442beae9213e5e5a85859f745ee90
Jo-Philipp Wich <jow@openwrt.org>
2015-02-10 12:21:55 +0100

    pkgupdate-watch.sh: show idle and pending states
    Signed-off-by: Jo-Philipp Wich <jow@openwrt.org>

--- pkgupdate-watch.sh
+++ pkgupdate-watch.sh
@@ -14,12 +14,20 @@ while true; do
 			sort -nr | sed -ne '1s/^[0-9.]\+ //p')"
 
 		if [ -d "$log" ]; then
-			msg="$(tail -n1 "$log/compile.txt")"
-			if [ ${#msg} -gt 80 ]; then
-				msg="${msg:0:80}"
+			d1=$(date +%s)
+			d2=$(date +%s -r "$log/compile.txt")
+
+			if [ $(($d1 - $d2)) -gt 5 ]; then
+				log="- idle -"
+				msg=""
+			else
+				msg="$(tail -n1 "$log/compile.txt")"
+				if [ ${#msg} -gt 80 ]; then
+					msg="${msg:0:80}"
+				fi
 			fi
 		else
-			log="-"
+			log="- pending -"
 			msg=""
 		fi
 
